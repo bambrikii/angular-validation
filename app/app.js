1e8216931c4590ca669f344457c3654afd4657f7
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2015-12-08 21:49:04 +0300

    - angular dependency

--- app/app.js
+++ app/app.js
@@ -1,17 +1,28 @@
 angular.module("app", [])
-.directive("txt", function() {
-    return {
-	scope: {
-	    required: "="
-	},
-	templateUrl: "app/txt.htm",
-	controller: function($scope) {
-	}
-    }
-})
-.controller("controller1", function($scope) {
-    $scope.formName = "form1";
-    $scope.elementName = "elem1";
-    console.log($scope.form1);
-})
+	.directive("txt", function () {
+		return {
+			scope: {
+				required: "=",
+				model: "=",
+				pattern: "=",
+				formName: "=",
+				elementName: "="
+			},
+			templateUrl: "app/txt.htm",
+			controller: function ($scope) {
+				//console.log($scope.$parent.form1);
+				console.log($scope.$parent[$scope.formName][$scope.elementName]);
+			}
+		}
+	})
+	.controller("controller1", function ($scope) {
+		$scope.formName = "form1";
+		$scope.elementName = "elem1";
+		$scope.elementName2 = "elem2";
+		$scope.someValue = "someValue1";
+		$scope.someValue2 = "someValue2";
+		$scope.somePattern = /^\d+$/;
+		console.log($scope);
+		//$scope[$scope.formName][$scope.elementName].$setValidity($scope.elementName, false);
+	})
 ;
